55ebe89f129c4705336b3c2af90f214778b74e4a
CringePopo <nymous293@gmail.com>
2023-10-24 12:32:08 +0000

    Added article page

---
+++ What Akufo-Addo really told Akosombo Dam flood victims in the Volta Region that has sparked outrage_files/3FykFDuoa50.js
@@ -0,0 +1,4 @@
+;/*FB_PKG_DELIM*/
+
+__d("coerceImageishSprited",[],(function(a,b,c,d,e,f){"use strict";function a(a){if(!a||typeof a!=="object"||!a.sprited)return null;return a.sprited===1?{type:"css",className:a.spriteMapCssClass+" "+a.spriteCssClass,identifier:a.loggingID}:{type:"cssless",style:{backgroundImage:"url('"+a.spi+"')",backgroundPosition:a.p,backgroundSize:a.sz,width:a.w+"px",height:a.h+"px",backgroundRepeat:"no-repeat",display:"inline-block"},identifier:a.loggingID}}f["default"]=a}),66);
+__d("coerceImageishURL",[],(function(a,b,c,d,e,f){"use strict";function a(a){if(a&&typeof a==="object"&&!a.sprited&&typeof a.uri==="string"&&a.width!==void 0&&a.height!==void 0)return a;else return null}f["default"]=a}),66);
